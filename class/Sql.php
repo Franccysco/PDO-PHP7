07164deb75cf12b24c32fe19cf397facb6b18a3c
Francisco Sousa <fchagas.sousa45@gmail.com>
2018-02-15 16:54:05 -0300

    adic usuario

--- class/Sql.php
+++ class/Sql.php
@@ -9,14 +9,14 @@ class Sql extends PDO{
 	}
 
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key,$value);
+			$this->setParam($statement,$key,$value);
 		}
 	}
 
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 
 	public function query($rawquery,$params = array()){
@@ -26,6 +26,7 @@ class Sql extends PDO{
 
 		$stmt->execute();
 
+		//var_dump($stmt);
 		return $stmt;
 
 	}
@@ -35,7 +36,7 @@ class Sql extends PDO{
 
 		$stmt = $this->query($rawquery,$params);
 
-		var_dump($stmt);
+		//var_dump($stmt);
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 	}
